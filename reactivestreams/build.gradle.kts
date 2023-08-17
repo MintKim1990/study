366495cd940eef11b023a8c410d223f8fc06e854
MintKim1990 <rlaalsxo1990@gmail.com>
2023-08-17 17:52:03 +0900

    feat: Netty EventGroup을 이용한 TCP 소켓서버 구현
     - Netty 사용을 위한 Dependency 추가
     - NIOEventGroup을 이용한 소켓서버 구현

--- reactivestreams/build.gradle.kts
+++ reactivestreams/build.gradle.kts
@@ -1,4 +1,8 @@
 dependencies {
+    // Lombok
     implementation("org.projectlombok:lombok:1.18.28")
     annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+    // Netty
+    implementation("io.netty:netty-all:4.1.24.Final")
 }
