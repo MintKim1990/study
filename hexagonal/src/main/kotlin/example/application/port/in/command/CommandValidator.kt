45f1a7983f56bfde4136a5fd5bf8ae64fdb32d76
MintKim1990 <rlaalsxo1990@gmail.com>
2023-09-18 09:54:40 +0900

    fix: 헥사고날 아키텍쳐 CommandValidator 로직 수정
     - 상위클래스 init 메서드 호출시 하위클래스에 파라미터 바인딩이 안된 상태로 들어와 Validation 하는 의미가 없으므로 추후 AOP등으로 처리하기위해 init 블럭에서 Validation 하는 로직 제거

--- hexagonal/src/main/kotlin/example/application/port/in/command/CommandValidator.kt
+++ hexagonal/src/main/kotlin/example/application/port/in/command/CommandValidator.kt
@@ -1,19 +1,11 @@
 package example.application.port.`in`.command
 
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
-import org.springframework.web.context.ContextLoader
 import javax.validation.ConstraintViolationException
 import javax.validation.Validation
 import javax.validation.Validator
 
 
-abstract class CommandValidator<T> {
-
-    private var validator: Validator = Validation.buildDefaultValidatorFactory().validator
-
-    init {
-        validate()
-    }
+abstract class CommandValidator<T>(private val validator: Validator = Validation.buildDefaultValidatorFactory().validator) {
 
     fun validate() {
         val violations = validator.validate(this as T)
