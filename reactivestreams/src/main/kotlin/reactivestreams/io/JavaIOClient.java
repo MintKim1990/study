164e7ebb1341ef77a21fda50f828e4e7e0591a13
MintKim1990 <rlaalsxo1990@gmail.com>
2024-12-15 12:22:21 +0900

    fix : JavaIO 코드 수정
    멀티스레드 방식으로 요청 및 응답하는 로직으로 수정

--- reactivestreams/src/main/kotlin/reactivestreams/io/JavaIOClient.java
+++ reactivestreams/src/main/kotlin/reactivestreams/io/JavaIOClient.java
@@ -8,25 +8,49 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class JavaIOClient {
 
+
+    private static final Integer PROCESSED_COUNT = 400;
     private static final String MESSAGE = "This is Client";
+    private static ExecutorService executorService = Executors.newFixedThreadPool(PROCESSED_COUNT);
 
     @SneakyThrows
     public static void main(String[] args) {
-        try (Socket socket = new Socket()) {
-            socket.connect(new InetSocketAddress("localhost", 8080));
 
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(MESSAGE.getBytes());
-            outputStream.flush();
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        long start = System.currentTimeMillis();
 
-            InputStream inputStream = socket.getInputStream();
-            byte[] responseBytes = new byte[1024];
-            inputStream.read(responseBytes);
-            log.info("response Body : {}", new String(responseBytes).trim());
+        for (int i = 0; i < PROCESSED_COUNT; i++) {
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                try (Socket socket = new Socket()) {
+                    socket.connect(new InetSocketAddress("localhost", 8080));
+
+                    OutputStream outputStream = socket.getOutputStream();
+                    outputStream.write(MESSAGE.getBytes());
+                    outputStream.flush();
+
+                    InputStream inputStream = socket.getInputStream();
+                    byte[] responseBytes = new byte[1024];
+                    inputStream.read(responseBytes);
+                    log.info("response Body : {}", new String(responseBytes).trim());
+                } catch (Exception e) {}
+            }, executorService);
+
+            futures.add(future);
         }
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        executorService.shutdown();
+        long end = System.currentTimeMillis();
+        log.info("duration : {}", (end - start) / 1000);
+
     }
 }
--- reactivestreams/src/main/kotlin/reactivestreams/io/JavaIOServer.java
+++ reactivestreams/src/main/kotlin/reactivestreams/io/JavaIOServer.java
@@ -3,34 +3,57 @@ package reactivestreams.io;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class JavaIOServer {
 
+    private static final Integer PROCESSED_COUNT = 200;
     private static final String MESSAGE = "This is Server";
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(PROCESSED_COUNT);
 
     @SneakyThrows
     public static void main(String[] args) {
+
         try (ServerSocket serverSocket = new ServerSocket()) {
             serverSocket.bind(new InetSocketAddress("localhost", 8080));
 
             while(true) {
                 Socket clientSocket = serverSocket.accept(); // 블락킹으로 동작
 
-                byte[] requestBytes = new byte[1024];
-                InputStream inputStream = clientSocket.getInputStream();
-                inputStream.read(requestBytes);
-                log.info("request Body : {}", new String(requestBytes).trim());
-
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write(MESSAGE.getBytes());
-                outputStream.flush();
+                // 동시에 200개에 요청이 처리가능하며
+                // 사용자에 요청을 받아 1초가 걸리는 어떠한 작업을 수행 후 사용자에게 응답 리턴
+                executorService.submit(() -> {
+                    try {
+                        readRequest(clientSocket);
+                        Thread.sleep(1000);
+                        writeResponse(clientSocket);
+                    } catch (Exception e) {}
+                });
             }
+        } finally {
+            executorService.shutdown();
         }
     }
+
+    private static void readRequest(Socket clientSocket) throws IOException {
+        byte[] requestBytes = new byte[1024];
+        InputStream inputStream = clientSocket.getInputStream();
+        inputStream.read(requestBytes);
+        log.info("request Body : {}", new String(requestBytes).trim());
+    }
+
+    private static void writeResponse(Socket clientSocket) throws IOException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        outputStream.write(MESSAGE.getBytes());
+        outputStream.flush();
+    }
+
 }
